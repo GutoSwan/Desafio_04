754b82c25901601b85f6a9115bfa63eb4e2c920e
GutoSwan <gubuenosa@gmail.com>
2022-04-08 01:07:10 -0300

    desafio 4 concluido

---
+++ desafio4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using desafio4;
+
+Aluno orlandoAluno = new Aluno("orlando", 24, 9.5);
+
+Aluno scobyAluno = new Aluno("scoby", 22, 4);
+
+Aluno chubsAluno = new Aluno("chubs", 65, 10);
+
+List<Aluno> alunos = new List<Aluno>();
+alunos.Add(orlandoAluno);
+alunos.Add(scobyAluno);
+alunos.Add(chubsAluno);
+
+var somaDasNotas = alunos.Select(x => x.nota).Sum();
+Console.WriteLine(somaDasNotas);
+
+var somaFinal = alunos.ToArray().Select(x => x.nota).Sum();
+Console.WriteLine(somaFinal);
+Console.ReadKey();
